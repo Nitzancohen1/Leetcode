d467b804f7d5369881008aee12a3da7354a8865d
nitzancohen1 <87821971+nitzancohen1@users.noreply.github.com>
2021-11-02 00:31:53 +0200

    Time: 56 ms (69.58%), Space: 13.4 MB (38.87%) - LeetHub

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -5,8 +5,8 @@ class Solution(object):
 
 	inputNum = x
 	newNum = 0
-	while x>0:
-		newNum = newNum * 10 + x%10
-		x = x//10
-	return newNum == inputNum
+	while inputNum>0:
+		newNum = newNum * 10 + inputNum%10
+		inputNum = inputNum//10
+	return newNum == x
         
