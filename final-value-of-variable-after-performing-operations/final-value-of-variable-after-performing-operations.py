7d4f6f4493b8c78eda5ff5b08cf05a729decf84d
nitzancohen1 <87821971+nitzancohen1@users.noreply.github.com>
2021-11-02 00:35:05 +0200

    Time: 24 ms (99.77%), Space: 13.7 MB (11.96%) - LeetHub

--- final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def finalValueAfterOperations(self, operations):
-        """
-        :type operations: List[str]
-        :rtype: int
-        """
         X=0
         for i in range(len(operations)):
             if operations[i]=="--X" or operations[i]=="X--":
