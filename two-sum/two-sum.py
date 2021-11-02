0a3bcd0adf5c550b37b56ccc6400f705f7c3f041
Nitzancohen1 <87821971+Nitzancohen1@users.noreply.github.com>
2021-11-02 15:43:39 +0200

    Delete two-sum directory

--- two-sum/two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        dic1 = {}
-        for i,j in enumerate(nums): 
-            if j in dic1:
-                return [dic1[j], i]
-            dic1[target-j] = i
-        
